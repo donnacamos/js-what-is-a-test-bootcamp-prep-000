f2762b1637cf3789f848bdf5a8fecbbcb61df0ca
Donna Amos <donnacamos.developer@gmail.com>
2019-02-05 14:41:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ var message = `${name} is ${height} inches tall`
 
 module.exports = {name, height, message}
 console.log(message)
+
+  describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(index.message).toInclude(index.name)
+      expect(index.message).toInclude(index.height)
+    })
+  })
